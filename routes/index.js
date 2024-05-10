a9d443c00616a545f99aea36c161c9591681c018
Siaw Yoon Kean <yoonkean.siaw@servicerocket.com>
2024-05-10 17:14:08 +0800

    NEW

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express King' });
+	res.render('index', { title: 'Express King NEW' });
 });
 
 module.exports = router;
